d210ac8004573f2d4faf43f09cb38d0440bf1cce
mamtasonartech <sonarprasadtech@gmail.com>
2026-02-23 17:09:19 -0500

    Created the Questions and print them.

--- src/com/mamta/Quiz/Main.java
+++ src/com/mamta/Quiz/Main.java
@@ -3,5 +3,9 @@ package com.mamta.Quiz;
 public class Main {
     public static void main(String[] args) {
 
+        QuestionService service = new QuestionService();
+
+        service.display();
+
     }
 }
--- src/com/mamta/Quiz/QuestionService.java
+++ src/com/mamta/Quiz/QuestionService.java
@@ -2,7 +2,21 @@ package com.mamta.Quiz;
 
 public class QuestionService {
 
+    Question[] question = new Question[5];
+
+    public QuestionService(){
+        question[0] = new Question(1,"size of int","2","6","4","8","4");
+        question[1] = new Question(2,"size of double","2","6","4","8","8");
+        question[2] = new Question(3,"size of char","2","6","4","8","2");
+        question[3] = new Question(4,"size of long","2","6","4","8","8");
+        question[4] = new Question(5,"size of boolean","1","2","4","8","1");
+    }
 
 
+    public void display(){
+       for(Question que: question){
+           System.out.println(que.getQuestion());
+       }
+    }
 
 }
